04a851c9e642d3d9b812ee447bbe90cfad2d3e18
riserishitha <rishitha.n@kalvium.community>
2023-12-02 22:20:26 +0530

    commit changes

---
+++ app.js
@@ -0,0 +1,33 @@
+var listOf = [];
+var input = document.getElementById("input");
+var todo = document.getElementById("todolist");
+
+document.getElementById("button")
+button.addEventListener("click", () => {
+  listOf.push(input.value);
+  console.log(listOf);
+  input.value = "";
+  showList();
+});
+
+function showList() {
+  todo.innerHTML = listOf.map((item, i) => `
+    <li>
+      ${item}
+      <a onclick="editItem(${i})">Edit</a>
+      <a onclick="deleteItem(${i})">&times; | </a>
+    </li>`
+  ).join('');
+}
+
+function deleteItem(k) {
+  listOf.splice(k, 1);
+  showList();
+}
+
+function editItem(j) {
+var Value = prompt("Make the changes you want");
+  listOf.splice(j, 1, Value);
+  showList();
+}
+
